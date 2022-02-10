d116b9cd805fa62a31010097c6144972ea8e1383
iseekTo <1214046231@qq.com>
2022-02-10 17:59:18 +0800

    init: initial project by nextJs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# brisk-ui
-React component library for the user experience
+<div align="center">
+<h2>Brisk-ui</h2>
+
+[![Status](https://img.shields.io/badge/License-MIT-blue)]()
+
+<p>React component library for the user experience</p>
+
+</div>
+
+---
---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Brisk UI</title>
+        <meta
+          name="description"
+          content="Brisk UI: React component library for the user experience"
+        />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <span>todo</span>
+    </>
+  );
+};
+
+export default Home;
